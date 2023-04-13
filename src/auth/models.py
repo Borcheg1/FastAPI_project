242fb79b2adb4c80f01cd476205b97b351cd27c4
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-04-13 16:52:16 +0300

    added tablename and fixed ForeignKey

--- src/auth/models.py
+++ src/auth/models.py
@@ -2,19 +2,19 @@ from datetime import datetime
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import String, Boolean, Integer, TIMESTAMP, ForeignKey, JSON
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 
-
-class Base(DeclarativeBase):
-    pass
+from src.database import Base
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(length=320), nullable=False)
     email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
     registered_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.utcnow)
-    role_id: Mapped[int] = mapped_column(Integer, ForeignKey(role.c.id))
+    role_id: Mapped[int] = mapped_column(Integer, ForeignKey("role.id"))
     hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
@@ -22,6 +22,8 @@ class User(SQLAlchemyBaseUserTable[int], Base):
 
 
 class Role(Base):
+    __tablename__ = "role"
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(length=320), nullable=False)
     permissions: Mapped[JSON] = mapped_column(String(length=320), nullable=False)
