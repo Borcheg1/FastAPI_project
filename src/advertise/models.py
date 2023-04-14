5d34d3a0ce20d3f4dc8cce1453e147dd5c644e79
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-04-14 21:33:51 +0300

    added timezone=True into Advertise.created_at

--- src/advertise/models.py
+++ src/advertise/models.py
@@ -13,6 +13,6 @@ class Advertise(Base):
     name: Mapped[str] = mapped_column(String(length=320), nullable=False)
     address: Mapped[str] = mapped_column(String(length=500), nullable=False)
     price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=datetime.utcnow)
     description: Mapped[str] = mapped_column(Text)
     image_url: Mapped[str] = mapped_column(String(length=320))
