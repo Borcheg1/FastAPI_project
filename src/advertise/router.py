f34e05dd2ccb9d81de21b4b7b3595d738feb40c8
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-04-17 22:15:16 +0300

    renamed funcs; fixed returning into funcs like REST

--- src/advertise/router.py
+++ src/advertise/router.py
@@ -4,6 +4,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 
 from src.advertise.schemas import CreateAdvertise
+from src.auth.auth import fastapi_users
+from src.auth.models import User
 from src.database import get_async_session
 from src.advertise.models import Advertise
 
@@ -12,28 +14,51 @@ router = APIRouter(
     tags=["Advertise"]
 )
 
+current_user = fastapi_users.current_user()
+
 
 @router.get("/")
-async def get_advertise(max_price: int,
-                        limit: int = 2,
-                        offset: int = 0,
-                        session: AsyncSession = Depends(get_async_session)
-                        ):
+async def advertise(max_price: int,
+                    limit: int = 2,
+                    offset: int = 0,
+                    session: AsyncSession = Depends(get_async_session)
+                    ):
     query = select(Advertise).where(Advertise.price <= max_price).limit(limit).offset(offset)
     result = await session.execute(query)
     rows = [row[0] for row in result.all()]
     if rows:
-        return rows
+        return {
+            "status": "OK",
+            "data": rows,
+            "detail": None
+        }
     else:
-        return "Nothing found"
+        return {
+            "status": "OK",
+            "data": "Nothing found",
+            "detail": None
+        }
 
 
 @router.post("/")
-async def add_advertise(new_advertise: CreateAdvertise, session: AsyncSession = Depends(get_async_session)):
+async def advertise(
+        new_advertise: CreateAdvertise,
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user)
+):
     try:
         stmt = insert(Advertise).values(**new_advertise.dict())
         await session.execute(stmt)
         await session.commit()
-        return {"status": "success"}
+        return {
+            "status": "OK",
+            "data": None,
+            "detail": f"added {new_advertise.name}"
+        }
+
     except IntegrityError:
-        return f"This id already exists."
+        return {
+            "status": "error",
+            "data": None,
+            "detail": "This id already exists"
+        }
