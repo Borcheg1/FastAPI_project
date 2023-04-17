2f381775485b3ae7e56692da9554fce531e835de
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-04-17 22:11:25 +0300

    created router.py for sending email to users

---
+++ src/tasks/router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+
+from src.auth.auth import current_user
+from src.tasks.tasks import send_email
+
+
+router = APIRouter(prefix="/report")
+
+
+@router.get("/mail")
+def get_report_to_email(user=Depends(current_user)):
+    send_email.delay(user.username)
+    return {
+        "status": "OK",
+        "data": None,
+        "detail": "Message sent"
+    }
