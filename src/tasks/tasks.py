7acae66d23ebd426cf8ba20249af518cfedee2a0
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-05-01 08:58:49 +0300

    added new funcs for creating and sending emails

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -11,7 +11,7 @@ SMTP_PORT = 465
 celery = Celery("tasks", broker="redis://localhost:6379")
 
 
-def create_email(username: str, user_mail: str):
+def create_verify_email(username: str, user_mail: str, token: str):
     email = EmailMessage()
     email["Subject"] = "Real Estate"
     email["From"] = SMTP_USER
@@ -19,7 +19,26 @@ def create_email(username: str, user_mail: str):
 
     email.set_content(
         "<div>"
-        f'''<h1>Здравствуйте, {username}!</h1>'''
+        f'''<h2>Здравствуйте, {username}!</h2>'''
+        f'''<p>Для подтверждения электронной почты перейдите по ссылке</p>'''
+        f'''<a href='http://127.0.0.1:8000/auth/verify?token={token}' target='_blank'>Подтвердить электронную почту</a>'''
+        "</div>",
+        subtype="html"
+    )
+    return email
+
+
+def create_reset_pass_email(username: str, user_mail: str, token: str):
+    email = EmailMessage()
+    email["Subject"] = "Real Estate"
+    email["From"] = SMTP_USER
+    email["To"] = user_mail
+
+    email.set_content(
+        "<div>"
+        f'''<h2>Здравствуйте, {username}!</h2>'''
+        f'''<p>Ваш токен для смены пароля:</p>'''
+        f'''<p>{token}</p>'''
         "</div>",
         subtype="html"
     )
@@ -27,8 +46,16 @@ def create_email(username: str, user_mail: str):
 
 
 @celery.task
-def send_email(username: str, user_mail: str):
-    email = create_email(username, user_mail)
+def send_verify_email(username: str, user_mail: str, token: str):
+    email = create_verify_email(username, user_mail, token)
+    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
+        server.login(SMTP_USER, SMTP_PASS)
+        server.send_message(email)
+
+
+@celery.task
+def send_reset_pass_email(username: str, user_mail: str, token: str):
+    email = create_reset_pass_email(username, user_mail, token)
     with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
         server.login(SMTP_USER, SMTP_PASS)
         server.send_message(email)
